cd8a40259a62d6e79fb34a24eed04437ed622c43
Jacob Jäger <mail@jljaeger.de>
2019-03-13 17:30:56 +0100

    lewd neko + fixes

--- features/nsfw.py
+++ features/nsfw.py
@@ -13,12 +13,18 @@ class NSFW(Cog):
         em.set_image(url=await get_nekos_life("trap"))
         await ctx.send(embed=em)
 
-    @command("nsfwneko", help="traps aren't gay")
+    @command("lewdnekogif", help="get a nsfw neko gif")
     @is_nsfw()
-    async def neko(self, ctx: Context):
-        em = Embed(title="traps aren't gay", color=0xFF00AA)
-        em.set_image(url=await get_nekos_life("trap"))
+    async def lewdnekogif(self, ctx: Context):
+        em = Embed(color=0xFF00AA)
+        em.set_image(url=await get_nekos_life("nsfw_neko_gif"))
         await ctx.send(embed=em)
+
+    @command("lewdneko", help="get a nsfw neko")
+    @is_nsfw()
+    async def lewdneko(self, ctx: Context):
+        em = Embed(color=0xFF00AA)
+        em.set_image(url=await get_nekos_life("lewd"))
 
 
 def setup(bot):
