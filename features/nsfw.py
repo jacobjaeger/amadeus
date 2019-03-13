c6580ff455c9439ea0befa45b7db68b79b1e864d
Jacob Jäger <mail@jljaeger.de>
2019-03-13 17:24:42 +0100

    start + trap

--- features/nsfw.py
+++ features/nsfw.py
@@ -1,2 +1,18 @@
+from discord.ext.commands import Cog, command, Context, is_nsfw
+from discord import Embed
+from aiohttp import ClientSession
+from .common import invalid_arg, get_nekos_life
+from random import choice
+
+
+class NSFW(Cog):
+    @command("trap", help="traps aren't gay")
+    @is_nsfw()
+    async def trap(self, ctx: Context):
+        em = Embed(title="traps aren't gay", color=0xFF00AA)
+        em.set_image(url=await get_nekos_life("trap"))
+        await ctx.send(embed=em)
+
+
 def setup(bot):
-    pass
+    bot.add_cog(NSFW())
