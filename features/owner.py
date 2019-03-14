d23a0a196b2a693864eaaef3081608e14581f74f
Jacob Jäger <mail@jljaeger.de>
2019-03-14 17:26:13 +0100

    sh command

--- features/owner.py
+++ features/owner.py
@@ -1,8 +1,10 @@
 from discord.ext.commands import Cog, command, Context, is_owner
-from discord import Embed
+from discord import Embed, Message
 from aiohttp import ClientSession
 from .common import invalid_arg
 from random import choice
+import asyncio
+import async_timeout
 
 
 class Owner(Cog):
@@ -24,6 +26,30 @@ class Owner(Cog):
             title=str(ctx.bot.counter),
             color=0xFF00AA
         ))
+
+    @command("sh", hidden=True)
+    @is_owner()
+    async def sh(self, ctx: Context, *, prog):
+        msg: Message = await ctx.send(embed=Embed(
+            title="executing shell command...",
+            description=f"```\n$ {prog}\n```",
+            color=0xFF00AA
+        ))
+        try:
+            async with async_timeout.timeout(30) as cm:
+                proc = await asyncio.create_subprocess_shell(prog, stdout=asyncio.subprocess.PIPE,
+                                                             stderr=asyncio.subprocess.PIPE)
+                stdout, stderr = await proc.communicate()
+                NEWLINE = "\n"
+                body = f"```$ {prog}\n\n{str(stdout, encoding='utf-8')}\n{NEWLINE + '[stderr]' + NEWLINE + str(stderr, encoding='utf-8') + NEWLINE if stderr else ''}\n[exit code {proc.returncode}]```"
+        except asyncio.TimeoutError:
+            body = f"```$ {prog}\n\n[process timed out]```"
+        newem = Embed(
+            title="executed shell command",
+            description=body if len(body) <= 2048 else f"```$ {prog}\n\noutput too long...\n\n[exit code {proc.returncode}]```",
+            color=0xFF0000 if proc.returncode or cm.expired else 0x00FF00
+        )
+        await msg.edit(embed=newem)
 
 
 def setup(bot):
